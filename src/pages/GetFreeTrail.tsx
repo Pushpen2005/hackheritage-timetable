324ee97c6ee5f86d19823f361611a60935f95912
pushpen tiwari <pushpentiwari2005@gmail.com>
2025-08-16 12:42:29 +0530

    Create GetFreeTrail.tsx

---
+++ src/pages/GetFreeTrail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GetFreeTrial: React.FC = () => (
+  <div className="p-8">
+    <h1 className="text-3xl font-bold mb-4">Get a Free Trial</h1>
+    <p>Admins can request a free trial to explore premium features. Fill out the form below to get started.</p>
+    {/* Add form or CTA here as needed */}
+  </div>
+);
+
+export default GetFreeTrial;
